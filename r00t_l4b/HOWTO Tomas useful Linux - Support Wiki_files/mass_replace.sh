60b86d02d436e26c5d2aab2074a488135989e192
Tomas Overdrive Petru <over@labka.cz>
2016-11-14 08:03:38 +0100

    corp id removed

---
+++ r00t_l4b/HOWTO Tomas useful Linux - Support Wiki_files/mass_replace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+### this script will find and recursively replace http://overdrive.a-nihil.net for https://over23.github.io in all html files
+
+### good prerequisite is to install gnu-sed by e.g. brew
+
+#brew install gnu-sed
+#gsed -i -- 's/http:\/\/overdrive.a-nihil.net\//https:\/\/over23.github.io\//g' *.html
+#gsed -i -- 's/ľ/ž/g' *.html
+#gsed -i -- 's/ą/š/g' *.html
+#gsed -i -- 's/charset=ISO-8859-2/UTF-8/g' *.html
+gsed -i -- 's/http:\/\/terrewiki.apps.tmrk.corp/REPLACE_ME/' *
